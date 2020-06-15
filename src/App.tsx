cc93b66fff5368db342df1b2f2b700c4eec2e8f9
naari3 <naari.named@gmail.com>
2020-06-16 02:49:13 +0900

    Config eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <p>also hello tauriact</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -21,6 +22,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
